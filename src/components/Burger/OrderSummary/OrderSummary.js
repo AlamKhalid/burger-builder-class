54a40991b50d3582fe37b676537cdf587833165a
Alam <alamkhalid0803@gmail.com>
2020-05-31 03:07:50 +0500

    initial commit

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from "./../../UI/Button/Button";
+
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+    return (
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+        {props.ingredients[igKey]}
+      </li>
+    );
+  });
+  return (
+    <React.Fragment>
+      <h4>Your Order</h4>
+      <p>A delicious burger with following ingredients</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: ${props.price.toFixed(2)}</strong>
+      </p>
+      <p>Contine to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        Cancel
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        Contine
+      </Button>
+    </React.Fragment>
+  );
+};
+
+export default orderSummary;
